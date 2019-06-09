38e53e6bf21c53d9a15eeabd56e5afaad32957fd
Kyler Barber <kylerbarber820@yahoo.com>
2019-06-09 00:20:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-my_array=["wife", "kids", "husband"]
-my_array<< "dog"
+add_element_to_end_of_array(array, element).last("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
