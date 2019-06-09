d48c50a88bb3ffdce4c861b71947a51e2dc4595c
Kyler Barber <kylerbarber820@yahoo.com>
2019-06-09 00:25:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-add_element_to_end_of_array.push(5,2)
+add_element_to_end_of_array<<5
 end
 
 def add_element_to_start_of_array(array, element)
