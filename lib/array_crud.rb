9d7e0fe0a64a6ae85ec987741c66630b258d458e
Kyler Barber <kylerbarber820@yahoo.com>
2019-06-09 00:31:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-
+create_an_array<<5
 end
 
 def add_element_to_start_of_array(array, element)
-add_element_to_start_of_array.unshift("cats")
+
 end
 
 def remove_element_from_end_of_array(array)
